1fa7c64ea16ab8dab63363b1b98b9f4f778a94af
Nik <nikrom17@bitbucket.org>
2018-07-01 23:30:12 -0700

    fda

--- purchase.php
+++ purchase.php
@@ -1,10 +1,14 @@
 <?php
 
 session_start();
-$_SESSION['user']
+$_SESSION['user'];
 
-
+if (isset($_SESSION['user']))
+	$user = $_SESSION['user'];
+else
+	$user = 'no_user';
 $file_path = "carts/".$user.".csv";
-copy(
+copy($file_path, 'admin/') ;
+header('Location: index.php');
 
 ?>
